dac8b41e32a419605d6142fe6fe2f674cb84ec96
Alexander Barabanov <Sasha.bar99@gmail.com>
2020-12-16 20:57:11 +0300

    Main page with rates, login form

--- server/app.py
+++ server/app.py
@@ -1,32 +1,45 @@
-from flask import Flask, request, Response, json, jsonify
+from flask import Flask, request, Response, json, jsonify, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
 
 @app.route('/')
 def index_page():
     # rates will be here
-    return 'Index Page'
+    # TODO: fix attaching the index.js file to index.html
+    return render_template('index.html')
 
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration_page():
     return 'Registration Page'
 
 
-@app.route('/login')
-def login_page():
-    return 'Login Page'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('Formlogin.html', title='Sign In', form=form)
 
 
-@app.route('/account')
-def account_page():
-    return 'Account Page'
+@app.route('/profile',  methods=['GET'])
+def profile_page():
+    return 'Profile Page'
 
 
-@app.route('/cryptopackage')
-def cryptopackage_page():
-    return 'Cryptopackage Page'
+@app.route('/portfolio', methods=['GET'])
+def portfolio_page():
+    return 'Portfolio Page'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='127.0.0.1', port=5000, debug=True)
