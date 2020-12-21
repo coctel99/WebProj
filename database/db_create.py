6aa75653f1c9fc8b4fd36606dbc66f043fcb8582
Alexander Barabanov <Sasha.bar99@gmail.com>
2020-12-22 01:43:59 +0300

    Added SQLAlchemy, login and register

---
+++ database/db_create.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+engine = create_engine('sqlite:////database/crypto.db', echo=True)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True)
+    password = Column(String(24))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return "{}".format(self.username)
--- server/app.py
+++ server/app.py
@@ -1,5 +1,8 @@
+import flask
 from flask import Flask, request, Response, json, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from sqlalchemy import Column, String, Integer
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -7,13 +10,29 @@ from wtforms.validators import DataRequired
 class CustomFlask(Flask):
     jinja_options = Flask.jinja_options.copy()
     jinja_options.update(dict(
-        variable_start_string='%%',  # Default is '{{', I'm changing this because Vue.js uses '{{' / '}}'
+        variable_start_string='%%',  # Default is '{{', I'm changing this because Vue.js uses '{{' and '}} too'
         variable_end_string='%%',
     ))
 
 
 app = CustomFlask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/crypto.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True)
+    password = Column(String(24))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return "{}".format(self.username)
 
 
 class LoginForm(FlaskForm):
@@ -26,31 +45,50 @@ class LoginForm(FlaskForm):
 @app.route('/')
 def index_page():
     # rates will be here
-    # TODO: fix attaching the index.js file to index.html
     return render_template('index.html')
 
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        # TODO: check if exists
+
+        db_add_user(username, password)
+        return flask.redirect('/profile')
+
     return render_template('formregister.html', title='Registration', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        return flask.redirect('/profile')
     return render_template('formlogin.html', title='Sign In', form=form)
 
 
 @app.route('/profile',  methods=['GET'])
 def profile_page():
+    # TODO: profilization, add html
     return 'Profile Page'
 
 
 @app.route('/portfolio', methods=['GET'])
 def portfolio_page():
+    # TODO: add html
     return 'Portfolio Page'
 
 
+def db_add_user(username, password):
+    db.session.add(User(username, password))
+    db.session.commit()
+    print("DB: Created user ", username, password)
+
+
 if __name__ == '__main__':
+    #db.create_all()
     app.run(host='127.0.0.1', port=5000, debug=True)
